a914c1a554a24868d474152b8798dea745d983b6
ldl <849781856@qq.com>
2024-04-03 22:38:05 +0800

    debug

--- apps/wechat/wechat_route.py
+++ apps/wechat/wechat_route.py
@@ -4,7 +4,7 @@ from apps.wechat.wechat_handler import *
 blueprint_wechat = Blueprint(name="wechat", import_name="__name__", url_prefix="/wechat")
 
 
-@blueprint_wechat.route('/')
+@blueprint_wechat.route('')
 def check():
     return check_in(request)
 
