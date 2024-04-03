e72bc9fcdc2e13575b4a5705855eb14ab64a6955
ldl <849781856@qq.com>
2024-04-03 23:51:37 +0800

    debug

--- apps/wechat/wechat_route.py
+++ apps/wechat/wechat_route.py
@@ -1,12 +1,9 @@
-from flask import Blueprint,request
+from flask import Blueprint, request
 from apps.wechat.wechat_handler import *
 
 blueprint_wechat = Blueprint(name="wechat", import_name="__name__", url_prefix="/wechat")
 
 
-@blueprint_wechat.route('')
+@blueprint_wechat.route('', methods=["GET", "POST"])
 def check():
     return handle_request(request)
-
-
-
